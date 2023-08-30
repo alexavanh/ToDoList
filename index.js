b097238775ccddd5d93423d7d673a7eaaeee4156
Alexa Pham <alexapham01@gmail.com>
2023-08-29 22:43:36 -0700

    Implement list of tasks that user inputs

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
-//const arr = [];
+const dArr = [];
+const wArr = [];
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
-app.post("/", (req, res) => {
-    //arr.push()
+app.post("/submit", (req, res) => {
+    if (req.body["dTask"] != "") {
+        dArr.push(req.body["dTask"]);
+    }
+    if (req.body["wTask"] != "") {
+        wArr.push(req.body["wTask"]);
+    }
+    res.render("index.ejs", {
+        daysTasks: req.body["dTask"],
+        workTasks: req.body["wTask"],
+        dArr: dArr,
+        wArr: wArr
+    });
 });
 
 app.listen(port, () => {
